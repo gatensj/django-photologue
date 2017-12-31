36cc307f854e651601d76ffb7163c1fdeb58ec7d
Jeremy Gatens <jgatens@wharton.upenn.edu>
2017-12-30 20:12:02 -0500

    Fixed issue

--- photologue/models.py
+++ photologue/models.py
@@ -71,9 +71,12 @@ else:
 # Support CACHEDIR.TAG spec for backups for ignoring cache dir.
 # See http://www.brynosaurus.com/cachedir/spec.html
 PHOTOLOGUE_CACHEDIRTAG = os.path.join(PHOTOLOGUE_DIR, "photos", "cache", "CACHEDIR.TAG")
+
+'''
 if not default_storage.exists(PHOTOLOGUE_CACHEDIRTAG):
     default_storage.save(PHOTOLOGUE_CACHEDIRTAG, ContentFile(
         "Signature: 8a477f597d28d172789f06886806bc55"))
+'''
 
 # Exif Orientation values
 # Value 0thRow	0thColumn
